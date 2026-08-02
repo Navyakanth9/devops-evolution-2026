7fa78f802248d68128f8d58da0f1afd295864b25
navi <navyakanthnani@gmail.com>
2026-08-02 07:46:02 +0530

    Added First aws resource_Ec2

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "dev_server" {
+
+  ami = "ami-091b599f5f318ddd2"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "devops-evolution-2026"
+  }
+}
