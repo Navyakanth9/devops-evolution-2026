3b0fd76b7b088821845cef50b0a33ff419d70a49
navi <navyakanthnani@gmail.com>
2026-08-04 07:34:19 +0530

    Modified outputs line 3 to correct value

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "instance_public_ip" {
     description = "Public IP address of the EC2 instance"
-    value       = aws_instance.dev.server.public_ip
+    value       = aws_instance.dev_server.public_ip
 }
 
