e5ed7b67feadb35c3563d8efc0ad46fcc5729fa1
navi <navyakanthnani@gmail.com>
2026-08-08 12:16:47 +0530

    Module changes

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     bucket = "terraform-state-navyakanth-2026"
     key = "global/s3/terraform.tfstate"
     region ="us-east-1"
-    use_lockfile = true
+    use_lockfile = true // Enable state locking to prevent concurrent modifications
     encrypt = true
   }
 
@@ -19,27 +19,8 @@ terraform {
 #Use the variables defined in variables.tf
 provider "aws" {
   region = var.aws_region
+
 }
 
 
 
-#DYNAMIC DATA SOURCE: Automatically fetch the official, latest free-tier Amazon Linux 2023 AMI
-data "aws_ssm_parameter" "amazon_linux_2023" {
-  name = var.dynamic_ami
-}
-
-
-#Use variables defined in variables.tf
-#Create the instance using the dynamic AMI and modern t3.micro (Free Tier)
-resource "aws_instance" "dev_server" {
-  ami           = data.aws_ssm_parameter.amazon_linux_2023.value
-  instance_type = var.instance_type
-
-
-
-# Define tags for the instance using local variables and input variables
-  tags = {
-    Name = local.full_server_name
-    environment = var.environment_tag
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,29 +1,7 @@
 variable "aws_region" {
   type        = string
-  description = "The target AWS region for deployment"
-  default     = "us-east-1"
-}
+  description = "AWS region to deploy resources"
+  default     = "us-east-1" # Set a default so you don't have to define it at the root unless you want to override it!  
 
-variable "instance_type" {
-  type        = string
-  description = "The size of the EC2 instance"
-  default     = "t3.micro"
-}
-
-variable "environment_tag" {
-  type        = string
-  description = "Value for the Environment tag"
-  default     = "Development"
-}
-
-variable "dynamic_ami"{
-  type       = string
-  description = "Dynamic AMI ID for the EC2 instance"
-  default     = ""
-}
-
-locals{
-  project_name = "Evolution"
-  full_server_name = "${local.project_name}-server-${var.environment_tag}"
-
+  
 }
