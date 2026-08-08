ea2fb5d396ef5aaafa614896c7ed284e52ac1274
navi <navyakanthnani@gmail.com>
2026-08-08 12:21:34 +0530

    Emptyed all

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS region to deploy resources"
-  default     = "us-east-1" # Set a default so you don't have to define it at the root unless you want to override it!  
-
-  
-}
