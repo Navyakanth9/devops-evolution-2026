fcab9e37fe5977c1f6c3f9f5a638c9de0a677c78
navi <navyakanthnani@gmail.com>
2026-08-08 14:53:16 +0530

    Added variables.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ backend s3 {
 } 
 
 provider "aws" {
-  region = "ap-south-2"
+  region = var.aws_region
 }
 
 # resource block: This block defines the AWS resources to be created. In this case, it creates an EC2 instance with the specified AMI, instance type, and tags.
@@ -28,26 +28,26 @@ provider "aws" {
 # 1. THE BOUNDARY: Create the VPC
 
 resource "aws_vpc" "custom_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames   = true
 
-  tags = { Name = "evolution-vpc" }
+  tags = { Name = var.project_tags["Name"] }
 }
 
 #  The Room: Create Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.custom_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
 
-  tags = { Name = "evolution-public-subnet" }
+  tags = { Name = var.project_tags["Name"] }
 }
 
 # 3. The front door: Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.custom_vpc.id
 
-  tags = { Name = "evolution-igw" }
+  tags = { Name = var.project_tags["Name"] }
 }
 # 4. The path: Create Route Table
 resource "aws_route_table" "public_route_table" {
@@ -57,7 +57,7 @@ resource "aws_route_table" "public_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id 
   }
-  tags = { Name = "evolution-public-route-table" }
+  tags = { Name = var.project_tags["Name"] }
 }
 
 
@@ -93,37 +93,30 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
-  tags = { Name = "evolution-web-sg" }
+  tags = { Name = var.project_tags["Name"] }
 }
 
 # 6. THE AUTHENTICATION: Import your existing local Laptop PEM Key
 resource "aws_key_pair" "my_key" {
-  key_name   = "aws-pem"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgq1cjS4sqiX/x43v4HBR/sXaXdznlh4/0ccX0+SdxED/hTZFiL4N94veJ8fM4CNDZXLitpRRCNZK4f6k7OIFqMNpxSR2CG4I55tKUQsfPcOUvptEfojKE1B4NTiUnyIMgMhxgIABKSz3GpeeJ1SIa17k/6Dvgit6Le6vW2yymsiSYIDiHShVmWU9GRM2Lf70isQKSt802jKqOdDIqgK2cEIZo1bZPSS8BeaQ5ap/fZ6XPN6Fhmsj943yZ9DGQBteuv6KRdjcv5ixlEKrahVkQMpVhJ84xnR9CIiPoWRhxgqe5uvorazWDRuTlU1Uv2PID3nl/QGcbLX9/zv8lu/qb"
+  key_name   = var.key_pair_name
+  public_key = var.public_key_path
 }
 
 # 7. THE ENGINE: Fetch Latest Linux AMI
 data "aws_ssm_parameter" "amazon_linux_2023" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+  name = var.data_ssm_value
 }
 
 # 8. THE SERVER: Spin up EC2 inside the network with Apache installation
 resource "aws_instance" "web_server" {
   ami           = data.aws_ssm_parameter.amazon_linux_2023.value
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = aws_key_pair.my_key.key_name
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              echo "<h1>Welcome to the Apache Server hosted on AWS By Navyakanth !</h1>" | sudo tee /var/www/html/index.html
-              EOF
+  user_data = var.user_data
 
-  tags = { Name = "evolution-web-server" }
+  tags = { Name = var.project_tags["Name"] }
 }
---
+++ variables.tf
@@ -0,0 +1,75 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-2"
+}
+# --------------------------------------------------------------------------
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"  
+  }
+
+  variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet" 
+  type        = string
+  default     = "10.0.1.0/24"
+  }
+# --------------------------------------------------------------------------
+
+variable "data_ssm_value" {
+  description = "The name of the SSM parameter to retrieve" 
+  type        = string
+  default     = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64"
+  } 
+
+  variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t3.micro"
+  }
+# --------------------------------------------------------------------------
+  variable "key_pair_name" {
+  description = "The name of the key pair to use for the EC2 instance"
+  type        = string
+  default     = "aws-pem"
+  }
+
+variable "public_key_path" {
+  description = "The path to the public key file for the key pair"
+  type        = string
+
+  # defining the path to the public key file for the key pair. This is used to create the key pair in AWS.
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCgq1cjS4sqiX/x43v4HBR/sXaXdznlh4/0ccX0+SdxED/hTZFiL4N94veJ8fM4CNDZXLitpRRCNZK4f6k7OIFqMNpxSR2CG4I55tKUQsfPcOUvptEfojKE1B4NTiUnyIMgMhxgIABKSz3GpeeJ1SIa17k/6Dvgit6Le6vW2yymsiSYIDiHShVmWU9GRM2Lf70isQKSt802jKqOdDIqgK2cEIZo1bZPSS8BeaQ5ap/fZ6XPN6Fhmsj943yZ9DGQBteuv6KRdjcv5ixlEKrahVkQMpVhJ84xnR9CIiPoWRhxgqe5uvorazWDRuTlU1Uv2PID3nl/QGcbLX9/zv8lu/qb"
+  }
+  
+
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  } 
+
+  variable "user_data" {
+  description = "The user data script to run on instance launch"
+  type        = string
+  default     = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+                sudo yum install -y httpd mariadb-server
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                echo "<h1>Welcome to the Apache Server hosted on AWS By Navyakanth !</h1>" | sudo tee /var/www/html/index.html
+                EOF
+  }
+
+  variable "project_tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {
+    Name = "evolution"
+   } 
+  }
+
+
